dcdd87ac1cc8b493df36d2efd038cd0170e1ff4d
sainakarki <sainakarki100@gmail.com>
2025-01-20 15:03:40 +0545

    imgchange

---
+++ basic.js
@@ -0,0 +1,17 @@
+var number = parseInt (prompt("enter number1"));
+var number2 = parseInt(prompt("enter number 2"));
+
+var result = add(number, number2);
+alert(result);
+
+function add(x, y) {
+    return x + y;
+}
+/*
+if (number > 1) {
+    let number = 10;
+    document.write(number); //printf
+}
+document.write(number);
+*/
+
